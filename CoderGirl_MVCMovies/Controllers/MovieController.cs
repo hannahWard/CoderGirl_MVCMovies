75f349de55d057586d4b4eaca60287a239026265
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2019-05-04 08:08:56 -0500

    Missing Class Updates

--- CoderGirl_MVCMovies/Controllers/MovieController.cs
+++ CoderGirl_MVCMovies/Controllers/MovieController.cs
@@ -1,20 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
+using CoderGirl_MVCMovies.Data;
+using CoderGirl_MVCMovies.Models;
+
+
+
+
 
 namespace CoderGirl_MVCMovies.Controllers
 {
     public class MovieController : Controller
     {
-        public static Dictionary<int, string> movies = new Dictionary<int, string>();
+        public static IMovieRepository movieRepository = RepositoryFactory.GetMovieRepository();
         private static int nextIdToUse = 1; 
 
         public IActionResult Index()
         {
-            ViewBag.Movies = movies;
-            return View();
+            List<Movie> movies = movieRepository.GetMovies();
+            return View(movies);
         }
 
         [HttpGet]
@@ -24,7 +31,22 @@ namespace CoderGirl_MVCMovies.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(string movie)
+        public IActionResult Create(Movie movie)
+        {
+            movieRepository.Save(movie);
+            
+            return RedirectToAction(actionName: nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Movie movie = movieRepository.GetById(id);
+            return View("Create", movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Movie movie)
         {
             movies.Add(nextIdToUse, movie);
             nextIdToUse++;
