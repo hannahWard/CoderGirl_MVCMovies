07ee35bc7826acc013e8663161181a214f6dcaff
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2019-05-04 08:31:34 -0500

    Test

--- CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
+++ CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
@@ -9,20 +9,7 @@ namespace CoderGirl_MVCMovies.Controllers
 {
     public class MovieRatingController : Controller
     {
-        private IMovieRatingRepository repository = RepositoryFactory.GetMovieRatingRepository();
-
-        private string htmlForm = @"
-            <form method='post'>
-                <input name='movieName' />
-                <select name='rating'>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>                    
-                </select>
-                <button type='submit'>Rate it</button>
-            </form>";
+       private IMovieRatingRepository repository = RepositoryFactory.GetMovieRatingRepository();
 
        public IActionResult Index()
         {
