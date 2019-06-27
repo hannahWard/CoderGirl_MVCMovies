31699f23b064c2458cd5f3f939b51e4adf660b26
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2019-06-27 15:52:41 -0500

    After Class Commit

---
+++ CoderGirl_MVCMovies/Models/DirectorMovie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoderGirl_MVCMovies.Models
+{
+    public class DirectorMovie
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int DirectorId { get; set; }
+        public Director Director { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
